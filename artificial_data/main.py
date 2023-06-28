a4e70badac6987a39878b6bc26e1a9ed0221efc9
bibeauv <valerie.bibeau@polymtl.ca>
2023-06-28 19:49:52 -0400

    Proof of concept

---
+++ artificial_data/main.py
@@ -0,0 +1,95 @@
+from ode import *
+import matplotlib.pyplot as plt
+import torch
+from pinn import *
+
+y0 = np.array([1., 0., 0.2, 0.])
+
+class parameters():
+    k1 = 1.5
+    k2 = 0.5
+    k3 = 1.
+    k4 = 0.1
+
+prm = parameters()
+
+dt = 0.01
+tf = 10
+t, mat_y = runge_kutta(y0, prm, dt, tf)
+
+dt_pinn = 2.
+X, Y, idx = make_idx(dt_pinn, dt, t, mat_y, tf, 100, 0.2)
+idx_y0 = [0]
+
+device = torch.device('cpu')
+X, Y = put_in_device(X, Y, device)
+
+f_hat = torch.zeros(X.shape[0], 1).to(device)
+k = [0., 0., 0., 0.]
+PINN = Curiosity(X, Y, idx, idx_y0, f_hat, 1e-2, k, 10, 1, device)
+
+# Make all outputs positive
+for i, p in enumerate(PINN.PINN.parameters()):
+    p.data.clamp_(min=0.)
+
+epoch = 0
+max_epochs = 20000
+while epoch <= max_epochs:
+
+    try:
+
+        PINN.optimizer.step(PINN.closure)
+
+        PINN.PINN.k1.data.clamp_(min=0.)
+        PINN.PINN.k2.data.clamp_(min=0.)
+        PINN.PINN.k3.data.clamp_(min=0.)
+        PINN.PINN.k4.data.clamp_(min=0.)
+
+        epoch += 1
+
+        if epoch % 100 == 0:
+            print(f'Epoch {epoch} \t loss_c_data: {PINN.loss_c_data:.4e} \t loss_c_ode: {PINN.loss_c_ode:.4e}')
+
+        if epoch == 5000:
+            PINN.optimizer = torch.optim.Adam(PINN.params, lr=1e-3)
+
+        if epoch == 10000:
+            PINN.optimizer = torch.optim.Adam(PINN.params, lr=1e-4)
+
+    except KeyboardInterrupt:
+        break
+
+c_pred = PINN.PINN(X)
+t_pred = X.detach().numpy()
+cA_pred = c_pred[:,0].detach().numpy()
+cB_pred = c_pred[:,1].detach().numpy()
+cC_pred = c_pred[:,2].detach().numpy()
+cD_pred = c_pred[:,3].detach().numpy()
+
+t_train = X[idx].detach().numpy()
+cA_train = Y[idx,0].detach().numpy()
+cB_train = Y[idx,1].detach().numpy()
+cC_train = Y[idx,2].detach().numpy()
+cD_train = Y[idx,3].detach().numpy()
+
+plt.plot(t, mat_y[:,0], '--r')
+plt.plot(t, mat_y[:,1], '--b')
+plt.plot(t, mat_y[:,2], '--k')
+plt.plot(t, mat_y[:,3], '--c')
+plt.plot(t_pred, cA_pred, '-r')
+plt.plot(t_pred, cB_pred, '-b')
+plt.plot(t_pred, cC_pred, '-k')
+plt.plot(t_pred, cD_pred, '-c')
+plt.plot(t_train, cA_train, 'or', label='[A]')
+plt.plot(t_train, cB_train, 'ob', label='[B]')
+plt.plot(t_train, cC_train, 'ok', label='[C]')
+plt.plot(t_train, cD_train, 'oc', label='[D]')
+plt.legend()
+plt.xlabel('Time (s)')
+plt.ylabel('Concentration (mol/L)')
+plt.savefig('main.png', dpi=600)
+
+print(f'k1 = {PINN.PINN.k1.detach().numpy()}')
+print(f'k2 = {PINN.PINN.k2.detach().numpy()}')
+print(f'k3 = {PINN.PINN.k3.detach().numpy()}')
+print(f'k4 = {PINN.PINN.k4.detach().numpy()}')
---
+++ artificial_data/pinn.py
@@ -0,0 +1,152 @@
+# Libraries
+import torch
+import torch.autograd as autograd
+import torch.nn as nn
+
+import numpy as np
+import os
+
+# Set seed
+torch.set_default_dtype(torch.float)
+torch.manual_seed(1234)
+np.random.seed(1234)
+
+# PINN architecture
+class PINeuralNet(nn.Module):
+
+    def __init__(self, device, k, neurons):
+
+        super().__init__()
+
+        self.activation = nn.Tanh()
+
+        self.f1 = nn.Linear(1, neurons)
+        self.f2 = nn.Linear(neurons, neurons)
+        self.f3 = nn.Linear(neurons, neurons)
+        self.out = nn.Linear(neurons, 4)
+
+        self.k1 = torch.tensor(k[0], requires_grad=True).float().to(device)
+        self.k2 = torch.tensor(k[1], requires_grad=True).float().to(device)
+        self.k3 = torch.tensor(k[2], requires_grad=True).float().to(device)
+        self.k4 = torch.tensor(k[3], requires_grad=True).float().to(device)
+
+        self.k1 = nn.Parameter(self.k1)
+        self.k2 = nn.Parameter(self.k2)
+        self.k3 = nn.Parameter(self.k3)
+        self.k4 = nn.Parameter(self.k4)
+
+    def forward(self, x):
+
+        if torch.is_tensor(x) != True:
+            x = torch.from_numpy(x)
+        
+        a = x.float()
+        
+        z_1 = self.f1(a)
+        a_1 = self.activation(z_1)
+        z_2 = self.f2(a_1)
+        a_2 = self.activation(z_2)
+        z_3 = self.f3(a_2)
+        a_3 = self.activation(z_3)
+
+        a_4 = self.out(a_3)
+        
+        return a_4
+
+# Full PINN to discover k
+class Curiosity():
+
+    def __init__(self, X, Y, idx, idx_y0, f_hat, learning_rate, k, neurons, regularization, device):
+        
+        def loss_function_ode(output, target):
+            
+            loss = torch.mean((output - target)**2)
+
+            return loss
+        
+        def loss_function_data(output, target):
+
+            loss = torch.mean((output[idx] - target[idx])**2)
+
+            return loss
+        
+        def loss_function_IC(output, target):
+
+            loss = torch.mean((output[idx_y0] - target[idx_y0])**2)
+
+            return loss
+        
+        self.PINN = PINeuralNet(device, k, neurons).to(device)
+
+        self.PINN.register_parameter('k1', self.PINN.k1)
+        self.PINN.register_parameter('k2', self.PINN.k2)
+        self.PINN.register_parameter('k3', self.PINN.k3)
+        self.PINN.register_parameter('k4', self.PINN.k4)
+
+        self.x = X
+        self.y = Y
+
+        self.loss_function_ode = loss_function_ode
+        self.loss_function_data = loss_function_data
+        self.loss_function_IC = loss_function_IC
+        self.f_hat = f_hat
+        self.regularization = regularization
+
+        self.device = device
+        self.lr = learning_rate
+        self.params = list(self.PINN.parameters())
+        self.optimizer = torch.optim.Adam(self.params, lr=self.lr)
+
+    def loss(self, x, y_train):
+
+        g = x.clone()
+        g.requires_grad = True
+        
+        y = self.PINN(g)
+        cA = y[:,0].reshape(-1,1)
+        cB = y[:,1].reshape(-1,1)
+        cC = y[:,2].reshape(-1,1)
+        cD = y[:,3].reshape(-1,1)
+
+        k1 = self.PINN.k1
+        k2 = self.PINN.k2
+        k3 = self.PINN.k3
+        k4 = self.PINN.k4
+        
+        grad_cA = autograd.grad(cA, g, torch.ones(x.shape[0], 1).to(self.device), \
+                                retain_graph=True, create_graph=True)[0]
+        grad_cB = autograd.grad(cB, g, torch.ones(x.shape[0], 1).to(self.device), \
+                                retain_graph=True, create_graph=True)[0]
+        grad_cC = autograd.grad(cC, g, torch.ones(x.shape[0], 1).to(self.device), \
+                                retain_graph=True, create_graph=True)[0]
+        grad_cD = autograd.grad(cD, g, torch.ones(x.shape[0], 1).to(self.device), \
+                                retain_graph=True, create_graph=True)[0]
+
+        self.loss_cA_ode = self.loss_function_ode(grad_cA + k1*cA - k2*cB*cC, self.f_hat)
+        self.loss_cB_ode = self.loss_function_ode(grad_cB - k1*cA + k2*cB*cC, self.f_hat)
+        self.loss_cC_ode = self.loss_function_ode(grad_cC - k1*cA + k2*cB*cC \
+                                                          + k3*cC - k4*cD, self.f_hat)
+        self.loss_cD_ode = self.loss_function_ode(grad_cD - k3*cC + k4*cD, self.f_hat)
+        
+        
+        self.loss_cA_data = self.loss_function_data(cA, y_train[:,0].reshape(-1,1))
+        self.loss_cB_data = self.loss_function_IC(cB, y_train[:,1].reshape(-1,1))
+        self.loss_cC_data = self.loss_function_data(cC, y_train[:,2].reshape(-1,1))
+        self.loss_cD_data = self.loss_function_IC(cD, y_train[:,3].reshape(-1,1))
+
+        self.loss_c_data = self.loss_cA_data + self.loss_cB_data + self.loss_cC_data + self.loss_cD_data
+        self.loss_c_ode = self.loss_cA_ode + self.loss_cB_ode + self.loss_cC_ode + self.loss_cD_ode
+        
+        self.total_loss = self.regularization * self.loss_c_ode + self.loss_c_data
+        
+        return self.total_loss
+    
+    def closure(self):
+
+        self.optimizer.zero_grad()
+        
+        loss = self.loss(self.x, self.y)
+        
+        loss.backward()
+        
+        return loss
